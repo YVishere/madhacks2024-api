6da15ed0db035c4c956a1729c2f9c3f5cc5ad7de
Aditya YV <adityayv802@gmail.com>
2024-11-09 17:44:12 -0600

    Simplified logic

--- Scraper.py
+++ Scraper.py
@@ -3,17 +3,17 @@ import httpx
 import numpy as np
 
 async def algorithm(subject):
-    arr = np.array([])
+    toRet = ""
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(subject)
             soup = BeautifulSoup(response.text, "html.parser")
             paragraphs = soup.find_all("p")
             for p in paragraphs:
-                arr.append(p.text)
-            return arr
+                toRet = toRet + p.get_text() + " "
+            return toRet
         except AttributeError:
-            return arr
+            return toRet
 
 def fix_url(st1):
     st1 = st1 + '_'
@@ -24,12 +24,6 @@ def fix_url(st1):
             continue
         st2 = st2 + st1[i:i + 1]
     return st2
-
-def arr_to_String(arr):
-    toRet = ""
-    for i in range(0, len(arr)):
-        toRet = toRet + arr[i] + " "
-    return toRet
 
 async def startScraping(subject):
 
@@ -47,6 +41,5 @@ async def startScraping(subject):
     else:    url = "https://en.wikipedia.org/wiki/" + subject
 
     toRet = await algorithm(url)
-    toRet = arr_to_String(toRet)
 
     return toRet
