f4dc801048d2508859186b5715f28406793dea59
Aditya YV <adityayv802@gmail.com>
2024-11-09 14:29:41 -0600

    API fixes to be deployable

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 import time
 from Scraper import startScraping
 
+import uvicorn
+
 app = FastAPI()
 
 @app.get("/scrape")
-def scrape_view(subject: str, result: str, fastScrape: bool = False):
+async def scrape_view(subject: str, result: str, fastScrape: bool = False):
     start_time = time.time()
-    scraped_data = startScraping(subject, result, fastScrape)
+    scraped_data = await startScraping(subject, result, fastScrape)
     time_taken = time.time() - start_time
 
     return {'subject': subject, 'result': result, 'time_taken': time_taken, 'data': scraped_data}
+
+@app.get("/")
+def home_view():
+    return "Hello world"
+
+if __name__ == "__main__":
+    uvicorn.run(app)
