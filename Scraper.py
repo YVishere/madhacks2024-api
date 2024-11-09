da4d318913e85146d9a6bc75deb778eb0d6879fa
Aditya YV <adityayv802@gmail.com>
2024-11-09 17:42:31 -0600

    Changed return type for Scraper

--- Scraper.py
+++ Scraper.py
@@ -2,35 +2,15 @@ from bs4 import BeautifulSoup
 import httpx
 import numpy as np
 
-def dontCheck(url):
-    if url.find("#") != -1:
-        return True
-    if url.find("/wiki/") == -1:
-        return True
-    if url.find(".jpg") != -1 or url.find(".ogg") != -1 or url.find(".svg") != -1 or url.find(".png") != -1 or url.find(".jpeg") != -1 or url.find(".gif") != -1:
-        return True
-    if url.find("Help:") != -1 or url.find(":") != -1:
-        return True
-    if url.find("https") != -1:
-        return True
-    if url.find("commons") != -1 or url.find("_(identifier)") != -1:
-        return True
-
-    return False
-
 async def algorithm(subject):
     arr = np.array([])
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(subject)
             soup = BeautifulSoup(response.text, "html.parser")
-            tags = soup.find(id="bodyContent").find_all("a")
-            for tag in tags:
-                s = tag.get('href', None)
-                if dontCheck(s):
-                    continue
-                ns = s[s.rfind("/") + 1:]
-                arr = np.append(arr, ns)
+            paragraphs = soup.find_all("p")
+            for p in paragraphs:
+                arr.append(p.text)
             return arr
         except AttributeError:
             return arr
@@ -45,10 +25,11 @@ def fix_url(st1):
         st2 = st2 + st1[i:i + 1]
     return st2
 
-def fix_array(arr):
-    uarr, uind = np.unique(arr, return_index=True)
-    uarr = uarr[uind.argsort()]
-    return uarr
+def arr_to_String(arr):
+    toRet = ""
+    for i in range(0, len(arr)):
+        toRet = toRet + arr[i] + " "
+    return toRet
 
 async def startScraping(subject):
 
@@ -65,7 +46,7 @@ async def startScraping(subject):
         enable_fArray = False
     else:    url = "https://en.wikipedia.org/wiki/" + subject
 
-    toRet_preFix = await algorithm(url)
-    if enable_fArray: toRet = fix_array(toRet_preFix)
+    toRet = await algorithm(url)
+    toRet = arr_to_String(toRet)
 
-    return toRet.tolist()
+    return toRet
