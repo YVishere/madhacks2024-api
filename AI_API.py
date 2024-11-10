702e5dfe5ee8012e79ec30298c82e8157d4e2c5d
Aditya YV <adityayv802@gmail.com>
2024-11-10 07:09:28 -0600

    Global model

--- AI_API.py
+++ AI_API.py
@@ -2,16 +2,18 @@ import google.generativeai as genai
 import newsScraper as ns
 from Scraper import process_text
 
+genai.configure(api_key="AIzaSyD1KcB-Swnmj7bITFZ4Wbln6rmhS7w9L10")
+global model
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 def input_prompt(x):
-    genai.configure(api_key="AIzaSyD1KcB-Swnmj7bITFZ4Wbln6rmhS7w9L10")
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    global model
     response = model.generate_content(x)
     return(response.text)
 
 async def news_handler(x):
-    genai.configure(api_key="AIzaSyD1KcB-Swnmj7bITFZ4Wbln6rmhS7w9L10")
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    global model
     prompt = "Answer in only in one or two words: What is the subject of the sentence in " + x + "?. Do not add any quotes or punctuation. And the subject must be a place, person or a proper noun."
     response = model.generate_content(prompt)
     articles = await ns.startScrapingNews(response.text)
