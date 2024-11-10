44312dde30b69e7ce7a185f981e337219302db00
Aditya YV <adityayv802@gmail.com>
2024-11-10 06:53:12 -0600

    Working news

--- AI_API.py
+++ AI_API.py
@@ -1,4 +1,6 @@
 import google.generativeai as genai
+import newsScraper as ns
+from Scraper import process_text
 
 
 def input_prompt(x):
@@ -6,3 +8,17 @@ def input_prompt(x):
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(x)
     return(response.text)
+
+async def news_handler(x):
+    genai.configure(api_key="AIzaSyD1KcB-Swnmj7bITFZ4Wbln6rmhS7w9L10")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    prompt = "Answer in only in one or two words: What is the subject of the sentence in " + x + "?. Do not add any quotes or punctuation. And the subject must be a place, person or a proper noun."
+    response = model.generate_content(prompt)
+    articles = await ns.startScrapingNews(response.text)
+    s = ""
+    for art in articles:
+        s = s + art + ";; "
+    prompt = "Here is a list of new articles about " + x + ". Read out only 5 of them now, and read the next 5 if you have been asked to in the next prompt and so on: " + s
+    response = model.generate_content(prompt)
+    toRet = process_text(response.text)
+    return(toRet)
--- newsScraper.py
+++ newsScraper.py
@@ -1,22 +1,25 @@
-from bs4 import BeautifulSoup
 import httpx
-import numpy as np
+from bs4 import BeautifulSoup
 
-async def algorithm(subject):
+async def algorithm(subject, sub):
     toRet = []
+    
+    # Send a GET request to the URL
     async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(subject)
-            soup = BeautifulSoup(response.text, "html.parser")
-            sites = soup.find_all("a", href=True)
-            for s in sites:
-                nresp = await client.get(s['href'])
-                nsoup = BeautifulSoup(fix_url2(nresp.text), "html.parser")
-                ns = nsoup.find_all("h1")
-                toRet = toRet.append(ns)
-            return toRet
-        except AttributeError:
-            return toRet
+        response = await client.get(subject)
+        # Parse the page source with BeautifulSoup
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        # Find headlines
+        tags = soup.find(id="site-content").find_all("a",href=True)
+        print(tags)
+        for head in tags:
+            h = head.find("h4")
+            if h:
+                headline = h.get_text()
+                toRet.append(headline)
+
+    return toRet
 
 def fix_url(st1):
     st1 = st1
@@ -29,32 +32,20 @@ def fix_url(st1):
     return st2
 
 def fix_url2(st1):
-    st2 = "https://news.google.com"+st1[1:]
+    st2 = "https://news.google.com" + st1[1:]
     return st2
 
-# def process_text(text):
-#     text = text.replace("\n", " ")
-#     text = text.replace("  ", " ")
-#     text = text.replace("\\\"", "\"")
-#     text = text.replace("ⓘ", "")
-#     text = text.replace("( )", "")
-#     text = text.replace("()", "")
-
-#     #Get rid of square brackets and anything inside them
-#     text = re.sub(r'\[.*?\]', '', text)
-#     text = ' '.join(text.split())
-#     return text
-
 async def startScrapingNews(subject):
-
     print("Start scraping", subject)
 
-    n = subject.find(' ')
-    if not n == -1:
-        subject = fix_url(subject)
-    
-    url = "news.google.com/search?q=" + subject
+    subject = subject.strip()
 
-    toRet = await algorithm(url)
+    n = subject.find(' ')
+    if n != -1:
+        subject = fix_url2(subject)
+    
+    url = "https://www.nytimes.com/search?query=" + subject.lower()
+
+    toRet = await algorithm(url, subject)
 
     return toRet
